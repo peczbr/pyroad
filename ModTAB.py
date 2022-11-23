9fa0971345c1f426dda6a7d19386f083ea515f99
peczbr <117056435+peczbr@users.noreply.github.com>
2022-11-23 18:49:26 -0300

    Add files via upload
    PY file that can make changes in the original EXCEL DATA of Brazillian Football (BR12 to BR22)
    Original data founded in: https://www.football-data.co.uk/brazil.php

---
+++ ModTAB.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 22 15:37:13 2022
+
+@author: pecz
+"""
+import pandas as pd
+import datetime as dt
+import pytz as pz
+from zoneinfo import ZoneInfo as zf
+
+df = pd.read_excel('BRA.xlsx')
+#Padronizando nomes dos clubes
+df = df.apply(lambda x: x.replace("Atletico-PR","Athletico-PR"))
+df = df.apply(lambda x: x.replace("America MG","America-MG"))
+df = df.apply(lambda x: x.replace("Botafogo RJ","Botafogo-RJ"))
+df = df.apply(lambda x: x.replace("Flamengo RJ","Flamengo"))
+df = df.apply(lambda x: x.replace("Chapecoense-SC","Chapecoense"))
+df = df.apply(lambda x: x.replace("Atletico GO","Atletico-GO"))
+df = df.apply(lambda x: x.replace("H","V"))
+df = df.apply(lambda x: x.replace("D","E"))
+df = df.apply(lambda x: x.replace("A","D"))
+df = df.apply(lambda x: x.replace("Brazil","Brasil"))
+
+#Contador
+c = -1
+tam = len(df["Date"])-1
+#Acertando as datas para fuso horário UTC-3 (Sao_Paulo)
+while c !=  tam:
+    c = c + 1
+    w = df["Date"][c]
+    z = w.to_pydatetime()
+    ano = z.year
+    mes = z.month
+    dia = z.day
+    time = df["Time"][c]
+    dato = dt.datetime(ano,mes,dia,time.hour,time.minute)
+    data = dato.replace(tzinfo=zf("Europe/London"))
+    #convertendo hora
+    fuso2 = pz.timezone("America/Sao_Paulo")
+    cv = data.astimezone(fuso2)
+    dati = dt.datetime(cv.year,cv.month,cv.day,cv.hour,cv.minute)    
+    horario = dt.time(dati.hour,dati.minute)
+    data_j = dt.datetime(dati.year,dati.month,dati.day)
+    df["Time"][c] = horario
+    df["Date"][c] = data_j
+    
+df.to_excel("BR12-22.xlsx")
+ 
